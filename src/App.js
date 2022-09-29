fd53735146edf75bea92ef541f961a78fb12e669
unknown <borismujkovic@gmail.com>
2022-09-29 18:27:07 +0200

    finnal commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState, useContext, useEffect, } from 'react';
-import { HomePage } from './HomePage/HomePage';
-
+import React, { useState, useContext, useEffect } from "react";
+import { HomePage } from "./HomePage/HomePage";
 
 export const App = () => {
-
-
-
-
-
-  return <div>
-    <HomePage />
-  </div>
-}
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
